b3f571f6a456ff792628ea5f43bd92888685b558
MinhaLee94 <67772844+shjune10@users.noreply.github.com>
2020-09-21 07:44:47 -0400

    controllers and pug templete

---
+++ routers/videoRouter.js
@@ -0,0 +1,12 @@
+import express from "express"
+import routes from "../routes";
+
+const videoRouter = express.Router();
+
+videoRouter.get(routes.videos, (req, res) => res.render("videos"));
+videoRouter.get(routes.upload, (req, res) => res.render("upload"));
+videoRouter.get(routes.videoDetail, (req, res) => res.render("videoDetail"));
+videoRouter.get(routes.editVideo, (req, res) => res.render("editVideo"));
+videoRouter.get(routes.deleteVideo, (req, res) => res.render("deleteVideo"));
+
+export default videoRouter;
